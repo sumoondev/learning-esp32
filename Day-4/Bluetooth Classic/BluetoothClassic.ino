a59c99bc769ec5902944d63b603b231ee6caf9ed
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-11-20 12:43:51 +0000

    feat: Basic Bluetooth usage

---
+++ Day-4/Bluetooth Classic/BluetoothClassic.ino
@@ -0,0 +1,23 @@
+#include "BluetoothSerial.h"
+
+BluetoothSerial serialBT;
+char cmd;
+
+void setup() {
+  serialBT.begin("Esp32-BT");
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  if(serialBT.available()) {
+    cmd = serialBT.read();
+  }
+
+  if(cmd == '1') {
+    digitalWrite(2, HIGH);
+  }
+  if(cmd == '0') {
+    digitalWrite(2, LOW);
+  }
+  delay(20);
+}
